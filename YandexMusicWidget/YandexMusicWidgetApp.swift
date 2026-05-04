7080101ed363b283a8110947bf04ac22b5642da4
Tehnomag <tehnoseo@mail.ru>
2026-05-04 21:07:54 +0300

    Initial Commit

---
+++ YandexMusicWidget/YandexMusicWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  YandexMusicWidgetApp.swift
+//  YandexMusicWidget
+//
+//  Created by Артем Чирков on 04.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct YandexMusicWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
